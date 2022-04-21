745254d1afd4d95981e58820dd50631fc4da4feb
al0ny <alon.yafe@gmail.com>
2022-04-21 21:49:32 +0300

    Removed unused VARS file

--- vars.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-free_currency_api = "47fc2060-92a0-11ec-a7ae-75dd9f87b434"
-r = requests.get(url="https://freecurrencyapi.net/api/v2/latest?apikey=" + free_currency_api, verify=False)
